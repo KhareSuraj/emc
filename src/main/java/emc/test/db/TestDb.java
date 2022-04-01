8d2bb9b335e887904b247b2bca8558c149b2fa2c
KhareSuraj <91323702+KhareSuraj@users.noreply.github.com>
2022-04-01 15:43:56 +0300

    Removed the database connection class with errors

--- src/main/java/emc/test/db/TestDb.java
+++ src/main/java/emc/test/db/TestDb.java
@@ -50,7 +50,7 @@ public class TestDb extends HttpServlet {
 						String question = myRs.getString("question");
 						out.println(question);
 					}
-					
+					//result
 					
 					
 				}catch(Exception exc) {
