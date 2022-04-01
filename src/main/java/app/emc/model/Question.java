bfacba80552eb4bab09e65548ea6ef652cdca1ed
Deepan Gautam <dpngtm@gmail.com>
2022-04-01 14:40:05 +0300

    Added getter setter method for question model

--- src/main/java/app/emc/model/Question.java
+++ src/main/java/app/emc/model/Question.java
@@ -1,5 +1,32 @@
 package app.emc.model;
 
 public class Question {
+	
+	private int questionId;
+	private String question;
+	public Question(String question) {
+		this.question = question;
+	}
+	public Question(int questionId, String question) {
+		this.questionId = questionId;
+		this.question = question;
+	}
+	public int getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	
+	
+	
+	
 
 }
